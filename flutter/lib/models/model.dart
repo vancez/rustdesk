84def33f5f4f37e124083f23d2eb8c399a74dd20
Vance <vancevip5@gmail.com>
2026-02-07 15:48:57 +0800

    update

--- flutter/lib/models/model.dart
+++ flutter/lib/models/model.dart
@@ -2176,11 +2176,9 @@ class CanvasModel with ChangeNotifier {
     double w = size.width - leftToEdge - rightToEdge;
     double h = size.height - topToEdge - bottomToEdge;
     if (isMobile) {
-      // Account for safe area (notch, home indicator)
-      w = w - mediaData.padding.left - mediaData.padding.right;
+      // Account for safe area (notch)
       h = h -
           mediaData.padding.top -
-          mediaData.padding.bottom -
           mediaData.viewInsets.bottom -
           (parent.target?.cursorModel.keyHelpToolsRectToAdjustCanvas?.bottom ??
               0);
